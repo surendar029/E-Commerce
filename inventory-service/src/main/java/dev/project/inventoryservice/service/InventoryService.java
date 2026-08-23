519cd20d0b5b1bf24db38c83023e9e49c04c00e0
Surendar <surendarraja420@gmail.com>
2026-08-23 14:56:36 +0530

    feat: implement InventoryService to manage product stock levels and inventory state

--- inventory-service/src/main/java/dev/project/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/dev/project/inventoryservice/service/InventoryService.java
@@ -1,4 +1,58 @@
 package dev.project.inventoryservice.service;
 
+import dev.project.inventoryservice.dto.CreateInventoryRequest;
+import dev.project.inventoryservice.dto.InventoryResponse;
+import dev.project.inventoryservice.entity.Inventory;
+import dev.project.inventoryservice.exception.InventoryNotFoundException;
+import dev.project.inventoryservice.repository.InventoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    public InventoryService(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    public InventoryResponse getInventoryByProductId(Long productId) {
+        Inventory inventory = inventoryRepository.findByProductId(productId)
+                .orElseThrow(() ->
+                        new InventoryNotFoundException("Inventory not found for productId: " + productId));
+        return mapToResponse(inventory);
+    }
+
+    public InventoryResponse createInventory(CreateInventoryRequest request) {
+        if (inventoryRepository.existsByProductId(request.productId())) {
+            throw new IllegalStateException("Inventory already exists for product ID: " + request.productId());
+        }
+
+        Inventory inventory = new Inventory(
+                request.productId(),
+                request.initialQuantity(),
+                0
+        );
+
+        return mapToResponse(inventoryRepository.save(inventory));
+    }
+
+    public InventoryResponse addStock(Long productId, Integer quantity) {
+        Inventory inventory=inventoryRepository.findByProductId(productId).
+                orElseThrow(()->
+                        new InventoryNotFoundException(("Inventory not found for product ID: " + productId)));
+        inventory.setAvailableQuantity(inventory.getAvailableQuantity()+quantity);
+        return mapToResponse(inventoryRepository.save(inventory));
+    }
+
+    public InventoryResponse mapToResponse(Inventory inventory) {
+        return new InventoryResponse(
+                inventory.getProductId(),
+                inventory.getAvailableQuantity(),
+                inventory.getReservedQuantity(),
+                inventory.getAvailableQuantity() > 0
+        );
+    }
 }
